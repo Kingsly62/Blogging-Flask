8f7a001e02c4ae1928f700fb90aac12b3fb6866f
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-02-12 23:11:09 +0300

    add click event on button

--- static/JS/blog.js
+++ static/JS/blog.js
@@ -12,3 +12,34 @@ function read() {
     i = 0;
   }
 }
+
+var i = 0;
+function readHealth() {
+  if (!i) {
+    document.getElementById("readmorehealth").style.display = "inline";
+    document.getElementById("dotshealth").style.display = "none";
+    document.getElementById("readhealth").innerHTML = "Read Less";
+    i = 1;
+  } else {
+    document.getElementById("readmorehealth").style.display = "none";
+    document.getElementById("dotshealth").style.display = "inline";
+    document.getElementById("readhealth").innerHTML = "Read more";
+    i = 0;
+  }
+}
+
+
+var i = 0;
+function readPhone() {
+  if (!i) {
+    document.getElementById("readmorephone").style.display = "inline";
+    document.getElementById("dotsphone").style.display = "none";
+    document.getElementById("readphone").innerHTML = "Read Less";
+    i = 1;
+  } else {
+    document.getElementById("readmorephone").style.display = "none";
+    document.getElementById("dotsphone").style.display = "inline";
+    document.getElementById("readphone").innerHTML = "Read more";
+    i = 0;
+  }
+}
