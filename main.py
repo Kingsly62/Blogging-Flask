cbd94048384784be6cf7c8dc83ad7204223e3817
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-02-13 10:13:53 +0300

    add new files and new content

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    return render_template('home.html')
+
+
+@app.route('/blog')
+def blog():
     return render_template('blog.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post')
+def post():
+    return render_template('create_post.html')
 
 
 if __name__ == "__main__":
