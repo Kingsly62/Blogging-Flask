2acec0e133a9509842838325aec293efb1df5df1
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-02-16 02:03:33 +0300

    add validations in the signup and login form , create another decorators

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import email
 from flask import Flask, redirect, render_template, request, url_for
+import mysql.connector
 
 API_KEY = "http://api.quotable.io/random"
 app = Flask(__name__)
+
+conn = mysql.connector.connect(host="remotemysql.com",
+                               user="MBlhVgFB01",
+                               password="ksW9IhF4NV",
+                               database="MBlhVgFB01")
+
+cursor = conn.cursor()
 
 
 @app.route('/')
@@ -24,11 +33,40 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login')
 def login():
+    return render_template('Login.html')
+
+
+@app.route('/login_validate', methods=['GET', 'POST'])
+def login_validate():
     email = request.form.get('email')
     password = request.form.get('password')
-    return "The email is {} and the password is {}".format(email, password)
+
+    cursor.excute(
+        """SELECT * FROM `users` WHERE `email` LIKE '{}' AND `password` LIKE '{}'"""
+        .format(email, password))
+
+    users = cursor.fetchall()
+    if len(users) > 0:
+
+        return render_template('home.html')
+    else:
+        return render_template('Login.html')
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form.get('uname')
+    email = request.form.get('uemail')
+    password = request.form.get('upassword')
+
+    cursor.excute(
+        """INSERT INTO `users`, (`user_id`,`name`,`email`,`password`) VALUES(NULL,'{}','{}','{}')"""
+        .format(name, email, password))
+
+    conn.commit()
+    return "User registered successfully"
 
 
 @app.route('/post')
